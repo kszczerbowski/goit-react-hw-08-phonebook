2200811656e9bf644cde10367e82ced4aff0452d
kszczerbowski <kszczerbowski223@gmail.com>
2023-03-21 01:01:05 +0100

    changed notfoundpage image address

--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -5,7 +5,7 @@ export default function NotFoundPage() {
   return (
     <>
       <StyledImage
-        src="../../../public/pageNotFound.jpg"
+        src="https://github.com/kszczerbowski/goit-react-hw-08-phonebook/blob/main/public/pageNotFound.jpg?raw=true"
         alt="page not found"
       />
     </>
