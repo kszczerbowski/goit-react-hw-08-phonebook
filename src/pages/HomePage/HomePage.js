d9547631e975acc600c33edffd5371bdcdfa9a5b
kszczerbowski <kszczerbowski223@gmail.com>
2023-03-25 22:16:24 +0100

    new image for welcome page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { StyledHomeBox, StyledHomeMessage } from './HomePage.styled';
+import {
+  StyledHomeBox,
+  StyledHomeMessage,
+  StyledImage,
+} from './HomePage.styled';
 
 export default function HomePage() {
   return (
     <StyledHomeBox>
       <StyledHomeMessage>Welcome to the phonebook!</StyledHomeMessage>
-      <StyledHomeMessage>Please log in to access the app.</StyledHomeMessage>
+      <StyledImage
+        src="https://github.com/kszczerbowski/goit-react-hw-08-phonebook/blob/main/public/welcomeCat.jpg?raw=true"
+        alt="welcome image, cat using a phone book"
+      />
+      <StyledHomeMessage>Please log in to access the app 😸</StyledHomeMessage>
     </StyledHomeBox>
   );
 }
--- src/pages/HomePage/HomePage.styled.js
+++ src/pages/HomePage/HomePage.styled.js
@@ -1,9 +1,16 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const StyledHomeBox = styled.div`
-
-`
+  margin: 20px 15px;
+`;
 
 export const StyledHomeMessage = styled.h1`
+  margin: 20px 15px;
+`;
 
-`
+export const StyledImage = styled.img`
+  margin-top: 20px;
+  max-height: 80vh;
+  width: 70%;
+  aspect-ratio: 1.7;
+`;
